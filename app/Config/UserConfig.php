1fd35b9058d6aafc502e3ec179c58ada0b323ffb
sNakahodo <snakahodo55442@gmail.com>
2014-07-30 23:42:43 +1000

    added ignored files

--- app/Config/UserConfig.php
+++ app/Config/UserConfig.php
@@ -5,16 +5,16 @@
 class UserConfig {
 
   /* database username*/
-  public static $db_user = 'root';
-  //public static $db_user = 'anything_xplore';
+  //public static $db_user = 'root';
+  public static $db_user = 'cmsanyth_xplore';
 
   /* database password */
-  public static $db_pass = '';
-  //public static $db_pass = 'anythingnet123';
+  //public static $db_pass = '';
+  public static $db_pass = 'anythingnet123';
 
   /* database name */
-  public static $db_dbname = 'xploreworld';
-  //public static $db_dbname = 'anything_xploreworld';
+  //public static $db_dbname = 'xploreworld';
+  public static $db_dbname = 'cmsanyth_xploreworld';
 
   /* admin dashboard login username */
   public static $admin_user = 'test';
