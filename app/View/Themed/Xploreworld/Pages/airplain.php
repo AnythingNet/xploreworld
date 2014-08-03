ff3cc9f4f9167492a49217806d5a86054e57f293
sNakahodo <snakahodo55442@gmail.com>
2014-08-03 11:31:39 +1000

    added File.url to app/Config/bootstrap.php

--- app/View/Themed/Xploreworld/Pages/airplain.php
+++ app/View/Themed/Xploreworld/Pages/airplain.php
@@ -7,19 +7,19 @@
   
   <div class="row lights">  
 
-    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
       <div class="lightbulb"></div>
     </div>
 
-    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
       <div class="lightbulb"></div>
     </div>
 
-    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
       <div class="lightbulb"></div>
     </div>
 
-    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
       <div class="lightbulb"></div>
     </div>
 
@@ -27,49 +27,19 @@
 
   <div class="row window-body">  
 
-    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 window-column">
-      <div class="window">
-        <a href="http://127.0.0.1/xploreworld/map_sample">
-        <div class="window-frame inner"></div>
-        <div class="window-frame cover"></div>
-        <div class="window-frame outer"></div>
-          <?php echo $this->Html->image('/uploads/surfing.jpg', array('class' => 'window-content')); ?>
-        </a>
+    <?php foreach ($maps as $map) { ?>
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 window-column">
+        <div class="window">
+          <a href="<?php echo $map['Map']['url']; ?>">
+          <div class="window-frame inner"></div>
+          <div class="window-frame cover"></div>
+          <div class="window-frame outer"></div>
+          <div class="window-frame outer"></div>
+            <?php echo $this->Html->image($map['Media']['path'], array('class' => 'window-content')); ?>
+          </a>
+        </div>
       </div>
-    </div>
-
-    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 window-column">
-      <div class="window">
-        <a href="http://127.0.0.1/xploreworld/map_sample">
-        <div class="window-frame inner"></div>
-        <div class="window-frame cover"></div>
-        <div class="window-frame outer"></div>
-          <?php echo $this->Html->image('/uploads/beach.jpg', array('class' => 'window-content')); ?>
-        </a>
-      </div>
-    </div>
-
-    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 window-column">
-      <div class="window">
-        <a href="http://127.0.0.1/xploreworld/map_sample">
-        <div class="window-frame inner"></div>
-        <div class="window-frame cover"></div>
-        <div class="window-frame outer"></div>
-          <?php echo $this->Html->image('/uploads/historic.jpg', array('class' => 'window-content')); ?>
-        </a>
-      </div>
-    </div>
-
-    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 window-column">
-      <div class="window">
-        <a href="http://127.0.0.1/xploreworld/map_sample">
-        <div class="window-frame inner"></div>
-        <div class="window-frame cover"></div>
-        <div class="window-frame outer"></div>
-          <?php echo $this->Html->image('/uploads/nature.jpg', array('class' => 'window-content')); ?>
-        </a>
-      </div>
-    </div>
+    <?php } ?>
 
   </div> 
 
